b0aee5be1206447c096f7c00785f1feb74dc632f
joern@existsolutions.com <9az.xmJWY_d~>
2019-03-21 15:07:57 +0100

    moved import of common-styles upwards which fixes the menubar rendering

--- components/pb-collapse.js
+++ components/pb-collapse.js
@@ -1,10 +1,10 @@
 import { PolymerElement } from './assets/@polymer/polymer/polymer-element.js';
+import { html } from './assets/@polymer/polymer/lib/utils/html-tag.js';
+import { PbMixin } from './pb-mixin.js';
 import './assets/@polymer/iron-collapse/iron-collapse.js';
 import './assets/@polymer/iron-icons/iron-icons.js';
 import './assets/@polymer/iron-icon/iron-icon.js';
 import './assets/@polymer/iron-collapse/iron-collapse.js';
-import {PbMixin} from './pb-mixin.js';
-import { html } from './assets/@polymer/polymer/lib/utils/html-tag.js';
 /**
  * An iron-collapse with additional trigger section and optional expand/collapse icons.
  * Mainly used for the table of contents.
